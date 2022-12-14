f39a6f8ccbd3a8a9e4bc9f7c722e357f85ba3d2c
MRSlouzk <mrslouu@163.com>
2022-12-14 19:53:54 +0800

    Fix github actions error

--- spider/main.py
+++ spider/main.py
@@ -1,6 +1,6 @@
 # Python Script Created by MRS
-from spider.build_simulator import simulate_data_spider
-from spider.jinghao_rank import download_jinghao_rank
+from build_simulator import simulate_data_spider
+from jinghao_rank import download_jinghao_rank
 
 if __name__ == '__main__':
     simulate_data_spider()
