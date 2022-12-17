0ba1ddf749fffd324ba9ba3509631cea7b5f3884
MRSlouzk <mrslouu@163.com>
2022-12-17 16:30:32 +0800

    Fix path problem

--- spider/main.py
+++ spider/main.py
@@ -1,17 +1,17 @@
 # Python Script Created by MRS
 import asyncio
 
-from spider.build_simulator import simulate_data_spider
-from spider.jinghao_rank import download_jinghao_rank
-from spider.ship_icon import ship_icon_download
-from spider.japan_ship_name import download_japan_ship_contrast
+from build_simulator import simulate_data_spider
+from jinghao_rank import download_jinghao_rank
+from ship_icon import ship_icon_download
+from japan_ship_name import download_japan_ship_contrast
 
 async def run():
     print("*****资源同步开始*****")
     await simulate_data_spider()  # 大建池数据
     await download_jinghao_rank()  # 井号榜同步
     await ship_icon_download()  #舰船图标下载
-    await download_jinghao_rank() # 重樱船名对照
+    await download_japan_ship_contrast() # 重樱船名对照
     print("*****资源同步完成*****")
 
 if __name__ == '__main__':
