1d74b506e124ced822c1d1e28d60b14b6c5b53ab
MRSlouzk <mrslouu@163.com>
2022-12-17 18:15:27 +0800

    修复github actions执行失败的问题 #8

--- spider/main.py
+++ spider/main.py
@@ -7,9 +7,9 @@ from japan_ship_name import download_japan_ship_contrast
 async def run():
     print("*****资源同步开始*****")
     await simulate_data_spider()  # 大建池数据
-    await download_jinghao_rank()  # 井号榜同步
-    await ship_icon_download()  #舰船图标下载
-    await download_japan_ship_contrast() # 重樱船名对照
+    # await download_jinghao_rank()  # 井号榜同步
+    # await ship_icon_download()  #舰船图标下载
+    # await download_japan_ship_contrast() # 重樱船名对照
     print("*****资源同步完成*****")
 
 if __name__ == '__main__':
